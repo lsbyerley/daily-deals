65151054706f67618f2215bd8ddca65b8e616e15
lsbyerley <lsbyerley@gmail.com>
2024-04-29 21:50:40 -0400

    fix: typo

--- src/app/create/business/success/page.tsx
+++ src/app/create/business/success/page.tsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 export default async function CreateBusinessSuccess() {
   return (
     <div className='w-full flex flex-col items-center'>
-      <div className='animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-8 lg:py-16 text-foreground w-full'>
-        <h2 className='text-lg font-bold text-center'>Create Business</h2>
+      <div className='animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground w-full'>
         <div className='w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent' />
+
         <div className='flex flex-col gap-8 text-foreground w-full'>
+          <h2 className='text-lg font-bold text-center'>Create Business</h2>
           <p className="text-center">Business Created Successfully!</p>
           <p className='text-center'><Link href={'/create/business'} className='text-blue-400'>Create another</Link></p>
         </div>
--- src/app/create/deal/success/page.tsx
+++ src/app/create/deal/success/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default async function CreateBusinessSuccess() {
+export default async function CreateDealSuccess() {
   return (
     <div className='w-full flex flex-col items-center'>
       <div className='animate-in flex flex-col gap-14 opacity-0 max-w-4xl px-3 py-16 lg:py-24 text-foreground w-full'>
